a3e39a1916a005c1be59311f8d0c6c4ffbaf1fe3
ChagCode <85539626+ChagCode@users.noreply.github.com>
2023-03-23 13:59:05 +0300

    query test sql 2

--- SQL/test.sql
+++ SQL/test.sql
@@ -1,3 +1,4 @@
+-- найдем самолеты, которые летали только те дни, где из одного аэропорта таких самолетов вылетало более одного
 select  departure_airport,
 		scheduled_departure,
 		count_seats		
@@ -17,4 +18,15 @@ from (
 		 ) as subquery1 using(aircraft_code)
 	group by departure_airport, scheduled_departure
 	order by scheduled_departure) as subquery2
-where count_model > 1;
+where   count_model > 1;
+
+
+-- посмотрим на тех кто сделал бронь, но не прошел регистрацию
+select  t.ticket_no as ticket_no,
+		subquery1.flight_id as flight_id
+from tickets t
+left join (	select tf.ticket_no as ticket_no, tf.flight_id as flight_id, bp.boarding_no as boarding_no
+			from ticket_flights tf
+			join boarding_passes bp using(ticket_no, flight_id)
+		  ) as subquery1 using(ticket_no)
+where flight_id is null;
